26fa9d5a159c58db5af56db7a54ac924777df418
slexce <2767145231@qq.com>
2023-07-08 03:49:44 +0800

    timetabel update

--- EnjoyAnimation/schedute_lite.py
+++ EnjoyAnimation/schedute_lite.py
@@ -1,23 +1,31 @@
 import re,asyncio,threading
 from datetime import datetime,timedelta
 from typing import Callable
+import time
 
 class task_infor:
-    def __init__(self,type,time_units,func_name,func,run_time=None) -> None:
-        self.type:str=type
-        self.time_units:dict=time_units
+    def __init__(self,type:str,time_units:dict,func_name:str,func,run_time=None,task_id=None) -> None:
+        self.type=type
+        self.time_units=time_units
         self.run_time=run_time              #下次运行时间
         self.all_tick=None
-        self.func_name:str=func_name
+        self.func_name=func_name
         self.func=func
+        if task_id:
+            self.task_id=task_id
+        else:
+            self.task_id=id(func)
+        
 class schedute_lite:
     '''定时任务设置'''
     def __init__(self) -> None: 
         self.job_list=[]
         self.run_job_list=[]
-        self.thread=threading.Thread(target=self.task_run)
+        self.stop_tag=False
+        self.thread=threading.Thread(target=self.task_run(),name="timetable")
         self.thread.start()
-    def add_job(self,type:str,time_str:str):
+        
+    def add_job(self,type:str,time_str:str,task_id:int=None):
         '''type-->类型字符串:
             - （interval------------间隔执行）
             - （fixed----------固定时间执行）   
@@ -27,6 +35,10 @@ class schedute_lite:
             例子：
                 - 2021Y7M5d21h31m25s ----> 2021年7月5日21时31分25秒
                 - 7M23h20m ----------------------------------> 7月23时20分
+                
+            task_id-->添加任务的id
+                - 默认为对象id，比如id(func),但是当若干个任务引用同一个函数后重写时，则只返回最新的id
+                - 可自定义id
                 
             Use:
             >>> @schedute_lite.add_job(“date”,"2021Y7M5d21h31m25s") #在2021年7月5日21时31分25秒执行一次
@@ -50,7 +62,7 @@ class schedute_lite:
             '''函数装饰器'''
             async def add_job_2(*args, **kwargs):
                 return await func(*args, **kwargs)
-            self.job_list.append(task_infor(type,time_units,func.__name__,add_job_2))
+            self.job_list.append(task_infor(type,time_units,func.__name__,add_job_2,task_id=task_id))
             return add_job_2
         return add_job_1
 
@@ -59,6 +71,10 @@ class schedute_lite:
         while True:
             self.run_job_list,pop_list=[],[]
             task:task_infor
+            if self.stop_tag:
+                for t in self.job_list:
+                    t.run_time=None
+                break
             for item_i in range(len(self.job_list)):
                 task=self.job_list[item_i]
                 if not task.run_time:
@@ -98,15 +114,34 @@ class schedute_lite:
     async def start_run_job_list(self):
         '''运行列表'''
         while True:
+            if self.stop_tag:
+                break
             for task in self.run_job_list:
                 await task()
             await asyncio.sleep(0.5)
             
     def task_run(self):
         '''线程运行'''
-        loop=asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        tasks=[loop.create_task(self.appened_job_list()),loop.create_task(self.start_run_job_list())]
-        loop.run_until_complete(asyncio.gather(*tasks))
-        
+        def _task_run():
+            self.stop_tag=False
+            loop=asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            tasks=[loop.create_task(self.appened_job_list()),loop.create_task(self.start_run_job_list())]
+            loop.run_until_complete(asyncio.gather(*tasks))
+        if self.stop_tag:
+            self.thread=threading.Thread(target=_task_run,name="timetable")
+            self.thread.start()
+        else:
+            return _task_run
+    def task_stop(self):
+        '''停止任务'''
+        self.stop_tag=True
+    def remove_task(self,task_id:list | int):
+        '''移除任务'''
+        if isinstance(task_id,int):
+            task_id=[task_id]
+        for i in task_id:
+            if i not in [j.task_id for j in self.job_list]:
+                raise IndexError(f"无task_id为{i}的任务")
+        self.job_list=[i for i in self.job_list if i.task_id not in task_id]
 timetable=schedute_lite()
